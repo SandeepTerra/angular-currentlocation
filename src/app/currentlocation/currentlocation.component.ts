243b526c9a5941bb6057230c056ef2727253aa3f
SandeepTerra <Sandeep@terra.group>
2022-11-23 17:08:49 +0200

    call socket server

--- src/app/currentlocation/currentlocation.component.ts
+++ src/app/currentlocation/currentlocation.component.ts
@@ -2,6 +2,7 @@ import { Position } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserService } from '../user.service';
+import { io } from 'socket.io-client';
 
 @Component({
   selector: 'app-currentlocation',
@@ -31,7 +32,13 @@ export class CurrentlocationComponent implements OnInit {
         this.lat =pos.lat;
         this.lng =pos.lng;
         this.glink= "https://www.google.com/maps/?q=" + this.lat + "," + this.lng;
-        //console.log(`Positon: ${pos.lng} ${pos.lat}`);
+        
+        var latlng = { lat: this.lat, lng: this.lng };
+
+        //var socket = io.connect("http://localhost:3000");
+        const socket = io("http://localhost:3000");
+        socket.emit("coordinates", latlng);
+
       });
 
   }
